7fcedb02c506874a90ccf114ce9b745897059cac
potnuruaditi <55417361+potnuruaditi@users.noreply.github.com>
2022-09-28 22:57:46 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
-<!DOCTYPE html>
+<?php
+session_start();
+?>
+
 <html>
     <head>
         <title> LOGIN PAGE </title>
@@ -8,11 +11,20 @@
     <body>
         <div id="LoginPage">
             <h2 class="Header">Login Page</h2><br>    
-            <form action="index.php" method="post">  
+
+            <?php
+            if(isset($_SESSION["bad"]))
+                {
+                echo '<h2 class="Error"> Bad Credentials </h2>';
+                unset($_SESSION["bad"]);
+                }
+            ?>
+
+            <form action="front.php" method="post">  
                     <label for="UCID" id="Header2"><b>UCID</b></label>
-                    <input type="text" placeholder="Enter UCID" name="UCID" id="Header3" required>
+                    <input type="text" placeholder="Enter UCID" name="ucid" id="Header3" required>
                     <label for="Password" id="Header2"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="Password" id="Header3" required><br>
+                    <input type="password" placeholder="Enter Password" name="password" id="Header3" required><br>
                     <br><input type="submit" value="Login" name="LoginButton" id="Button"></br>
             </form>
         </div>
